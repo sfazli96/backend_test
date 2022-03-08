820ef03b1878829ef5313920adeeffeb7ceb8fb9
Sameh Fazli <sfazli96@gmail.com>
2022-03-07 20:10:31 -0800

    python microservice basic working

--- app.py
+++ app.py
@@ -3,5 +3,13 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
+def index():
     return "caio!"
+
+
+@app.route("/greet")
+def greet():
+    return "Good Morning"
+
+if __name__ == '__main__':
+    app.run(port=8500, debug=True)
