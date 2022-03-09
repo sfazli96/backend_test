43a9a1b8b6dbe68f224b392a7f0ac3d1c7a723f7
Sameh Fazli <sfazli96@gmail.com>
2022-03-08 23:36:18 -0800

    prints out data information GET Request

--- app.py
+++ app.py
@@ -2,14 +2,16 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return "caio!"
+items_table = [
+    {"id": 1, "vocab": "Love these cool toys at #ToysRUs. Go Check Them out", "sponsored": "ToysRUs", "advertisement": "PizzaHut"},
+    {"id": 2, "vocab": "Love these cool toys at #Walmart. Go Check Them out", "sponsored": "Walmart", "advertisement": "Mcdonald"},
+    {"id": 3, "vocab": "Love these cool toys at #Target. Go Check Them out", "sponsored": "Target", "advertisement": "Starbucks"},
+    {"id": 4, "vocab": "Love these cool toys at #BarnesNoble. Go Check Them out", "sponsored": "BarnesNoble", "advertisement": "LittleCaesars"},
+]
 
+@app.route("/products/<int:id>")
+def get_product_by_id(id):
+    product = items_table[id]
+    return "{} with an Ad by {} sponsored by {}".format(product["vocab"],product["advertisement"], product["sponsored"])
 
-@app.route("/greet")
-def greet():
-    return "Good Morning"
-
-if __name__ == '__main__':
-    app.run(port=8500, debug=True)
+    
